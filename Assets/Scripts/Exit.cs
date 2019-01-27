818456641e0c6a96051f915e66a922f044287743
Benjamin Armour <barmour@mail.pima.edu>
2019-01-27 14:32:58 -0700

    Lots of fixes, and added art for level

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
+
+	public string LevelToLoad;
+	
 	//Quits the game.
+
     public void doExitGame()
     {
         Application.Quit();
+    }
+
+    public void ChangeScene(){
+    	SceneManager.LoadScene(LevelToLoad);
     }
 
 }
